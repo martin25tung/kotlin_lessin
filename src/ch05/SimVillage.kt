a6e4c8d558fe2bcf93ab950e17dcb9421e7f615e
Martin Tung <martin25tung@gmail.com>
2020-05-07 15:11:18 +0800

    類型推斷

--- src/ch05/SimVillage.kt
+++ src/ch05/SimVillage.kt
@@ -2,7 +2,7 @@ package ch05
 
 fun main(args: Array<String>) {
 
-    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
