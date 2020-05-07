20586fe03fac37bb9a52c27e691bd2721ddb4c0d
Martin Tung <martin25tung@gmail.com>
2020-05-07 17:28:41 +0800

    Kotlin中的lambda就是閉包

--- src/ch05/SimVillage.kt
+++ src/ch05/SimVillage.kt
@@ -8,6 +8,7 @@ fun main(args: Array<String>) {
 fun runSimulation() {
     val greetingFunction = configureGreetingFunction()
     println(greetingFunction("Guyal"))
+    println(greetingFunction("Guyal"))
 }
 
 fun configureGreetingFunction(): (String) -> String{
