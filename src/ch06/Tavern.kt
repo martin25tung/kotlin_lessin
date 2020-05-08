e09cdd1d2f50c6afed52a27b2dff11a0780fa42c
Martin Tung <martin25tung@gmail.com>
2020-05-08 11:55:10 +0800

    空合並操作符跟let

--- src/ch06/Tavern.kt
+++ src/ch06/Tavern.kt
@@ -4,12 +4,16 @@ fun main(args: Array<String>) {
     var beverage = readLine()
 //    beverage = null
 
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("I can't do that without crashing - beerage was null!")
-    }
+//    if (beverage != null) {
+//        beverage = beverage.capitalize()
+//    } else {
+//        println("I can't do that without crashing - beerage was null!")
+//    }
+//
+//    val beverageServed: String = beverage ?: "Buttered Ale"
+//    println(beverageServed)
 
-    val beverageServed: String = beverage ?: "Buttered Ale"
-    println(beverageServed)
+    beverage?.let{
+        beverage = it.capitalize()
+    } ?: println("I can't do that without crashing - beerage was null!")
 }
