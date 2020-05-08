f26ac63197e8765c542f821b1bd04811939b3a5d
Martin Tung <martin25tung@gmail.com>
2020-05-08 13:57:03 +0800

    添加拋劍雜耍邏輯

---
+++ src/ch06/SwordJuggler.kt
@@ -0,0 +1,10 @@
+package ch06
+
+fun main(args: Array<String>) {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient) {
+        swordsJuggling = 2
+    }
+    println("You juggle $swordsJuggling swords!")
+}
