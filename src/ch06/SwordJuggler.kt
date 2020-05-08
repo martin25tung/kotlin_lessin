86c2379688bde210f548b214fd081fd13792063d
Martin Tung <martin25tung@gmail.com>
2020-05-08 14:04:33 +0800

    拋出異常

--- src/ch06/SwordJuggler.kt
+++ src/ch06/SwordJuggler.kt
@@ -1,11 +1,18 @@
 package ch06
 
+import java.lang.IllegalStateException
+
 fun main(args: Array<String>) {
     var swordsJuggling: Int? = null
     val isJugglingProficient = (1..3).shuffled().last() == 3
     if (isJugglingProficient) {
         swordsJuggling = 2
     }
+    proficiencyCheck(swordsJuggling)
     swordsJuggling = swordsJuggling!!.plus(1)
     println("You juggle $swordsJuggling swords!")
+}
+
+fun proficiencyCheck(swordsJuggling: Int?){
+    swordsJuggling ?: throw IllegalStateException("Player cannot juggle swords")
 }
