90fc817dc493b943b1d3c4d8766174aee9be05be
Martin Tung <martin25tung@gmail.com>
2020-05-08 11:06:23 +0800

    安全調用操作符

--- src/ch06/Tavern.kt
+++ src/ch06/Tavern.kt
@@ -1,7 +1,14 @@
 package ch06
 
 fun main(args: Array<String>) {
-    var beverage = readLine().capitalize()
+//    var beverage = readLine()?.capitalize()
 //    beverage = null
+    var beverage = readLine()?.let {
+        if (it.isNotBlank()){
+            it.capitalize()
+        }else {
+            "Buttered Ale"
+        }
+    }
     println(beverage)
 }
