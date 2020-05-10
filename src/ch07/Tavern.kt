341a223b360e6f12b03e3ea64b7b0bb545bf2cf6
Martin Tung <martin25tung@gmail.com>
2020-05-10 22:36:06 +0800

    字串操作

--- src/ch07/Tavern.kt
+++ src/ch07/Tavern.kt
@@ -14,4 +14,18 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "Madrigal buys a $name ($type) for $price"
     println(message)
+
+    val phrase = "Ah, delicious $name!"
+    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
+}
+
+private fun toDragonSpeak(phrase: String) = phrase.replace(Regex("[aeiou]")) {
+    when (it.value) {
+        "a" -> "4"
+        "e" -> "3"
+        "i" -> "1"
+        "o" -> "0"
+        "u" -> "|_|"
+        else -> it.value
+    }
 }
