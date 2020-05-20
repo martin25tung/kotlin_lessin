4a768b2982b0285b6cabf415177165175a0b2d2b
Martin Tung <martin25tung@gmail.com>
2020-05-20 14:34:30 +0800

    定義name屬性

--- src/ch12/Player.kt
+++ src/ch12/Player.kt
@@ -1,6 +1,8 @@
 package ch12
 
 class Player {
+    val name = "madrigal"
+
     fun castFireball(numFireball: Int = 2) =
         println("A glass of Fireball springs into existence.(x$numFireball)")
 }
