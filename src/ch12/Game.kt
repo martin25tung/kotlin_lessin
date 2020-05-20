2edc2c18a757f083256985ff44fe30f73aa57804
Martin Tung <martin25tung@gmail.com>
2020-05-20 16:33:34 +0800

    從main裡刪除兩個函數並添加在Player類

--- src/ch12/Game.kt
+++ src/ch12/Game.kt
@@ -17,20 +17,6 @@ fun main(args: Array<String>) {
     printPlayerStatus(auraColor, isBlessed, player.name, healthStatus)
 }
 
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
-    if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
-
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
-    100 -> "is in excellent condition!"
-    in 90..99 -> "has a few scratches."
-    in 75..89 -> if (isBlessed) {
-        "has some minor wounds, but is healing quite quickly!"
-    } else {
-        "has some minor wounds."
-    }
-    in 15..74 -> "looks pretty hurt."
-    else -> "is in awful condition!"
-}
 
 private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String) {
     println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "No"})")
--- src/ch12/Player.kt
+++ src/ch12/Player.kt
@@ -13,4 +13,19 @@ class Player {
 
     fun castFireball(numFireball: Int = 2) =
         println("A glass of Fireball springs into existence.(x$numFireball)")
+
+    private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
+        if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
+
+    private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
 }
