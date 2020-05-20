1a7f37a08036a77d4a9262bb586e1de87da4be71
Martin Tung <martin25tung@gmail.com>
2020-05-20 14:48:00 +0800

    自定義setter方法

--- src/ch12/Player.kt
+++ src/ch12/Player.kt
@@ -3,6 +3,9 @@ package ch12
 class Player {
     val name = "madrigal"
         get() = field.capitalize()
+        set(value) {
+            field = value.trim()
+        }
 
     fun castFireball(numFireball: Int = 2) =
         println("A glass of Fireball springs into existence.(x$numFireball)")
