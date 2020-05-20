89ab4060ece9353c18926d512c6a2b1560d4ee92
Martin Tung <martin25tung@gmail.com>
2020-05-20 14:44:14 +0800

    自定義getter方法

--- src/ch12/Player.kt
+++ src/ch12/Player.kt
@@ -2,6 +2,7 @@ package ch12
 
 class Player {
     val name = "madrigal"
+        get() = field.capitalize()
 
     fun castFireball(numFireball: Int = 2) =
         println("A glass of Fireball springs into existence.(x$numFireball)")
