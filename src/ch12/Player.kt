48564517e9756809f7b042de3af0875c35fd913f
Martin Tung <martin25tung@gmail.com>
2020-05-20 16:36:49 +0800

    刪除類函數不需要的參數

--- src/ch12/Player.kt
+++ src/ch12/Player.kt
@@ -14,10 +14,10 @@ class Player {
     fun castFireball(numFireball: Int = 2) =
         println("A glass of Fireball springs into existence.(x$numFireball)")
 
-    private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
+    private fun auraColor() =
         if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
 
-    private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
+    private fun formatHealthStatus() = when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
