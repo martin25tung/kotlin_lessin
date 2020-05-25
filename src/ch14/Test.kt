a1762f86353d3a4aaf48405b9bfdbcd78b1eb761
Martin Tung <martin25tung@gmail.com>
2020-05-25 14:14:21 +0800

    繼承-類型檢查做為條件分支

--- src/ch14/Test.kt
+++ src/ch14/Test.kt
@@ -1,9 +1,18 @@
 package ch14
 
+import java.lang.IllegalArgumentException
+
 fun main(args: Array<String>) {
     var room = Room("Foyer")
     println(room is TownSquare)
 
     var townSquare = TownSquare()
-    println(townSquare is Room)
+    var className = when(townSquare) {
+        is TownSquare -> "TownSquare"
+        is Room -> "Room"
+        else -> throw IllegalArgumentException()
+    }
+    println(className)
+
+
 }
