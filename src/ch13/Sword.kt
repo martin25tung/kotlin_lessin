5aad95a2ed09ef073da0daab2b59ad109a647e66
Martin Tung <martin25tung@gmail.com>
2020-05-25 11:07:04 +0800

    聖劍之謎-再次引用name屬性

--- src/ch13/Sword.kt
+++ src/ch13/Sword.kt
@@ -6,9 +6,15 @@ class Sword(_name: String) {
     set(value) {
         field = value.toLowerCase().reversed().capitalize()
     }
+
+    init {
+        name = _name
+    }
 }
 
 fun main(args: Array<String>) {
     val sword = Sword("Excalibur")
     println(sword.name)
+//    sword.name = "Gleipnir"
+//    println(sword.name)
 }
