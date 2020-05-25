9236d1d5110a05cb9481b1feae0283c2de70db87
Martin Tung <martin25tung@gmail.com>
2020-05-25 11:33:58 +0800

    繼承-打印方格描述

---
+++ src/ch14/Game.kt
@@ -0,0 +1,18 @@
+package ch14
+
+fun main(args: Array<String>) {
+    val player = Player("Madrigal")
+    player.castFireball()
+
+    var currentRoom = Room("Foyer")
+    println(currentRoom.description())
+    println(currentRoom.load())
+
+    // Player status
+    printPlayerStatus(player)
+}
+
+private fun printPlayerStatus(player: Player) {
+    println("(Aura: ${player.auraColor()}) (Blessed: ${if (player.isBlessed) "YES" else "No"})")
+    println("${player.name} ${player.formatHealthStatus()}")
+}
