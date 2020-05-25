cf736c608c761137ee275177079ead498ae5e9f3
Martin Tung <martin25tung@gmail.com>
2020-05-25 13:42:12 +0800

    繼承-為子類添加自訂義屬性和函數

--- src/ch14/Room.kt
+++ src/ch14/Room.kt
@@ -8,5 +8,9 @@ open class Room(val name: String) {
 
 class TownSquare : Room("Town Square") {
     override val dangerLevel = super.dangerLevel - 3
+    private var bellSound = "GWONG"
+
     override fun load() = "The villagers rally and cheer as you enter!"
+
+    private fun ringBell() = "The bell tower announces your arrival. $bellSound"
 }
