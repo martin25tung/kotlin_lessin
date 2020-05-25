8f1f4523eebe64a50a9627d90b91a0768ede8ded
Martin Tung <martin25tung@gmail.com>
2020-05-25 13:29:34 +0800

    繼承-定義TownSquare類

--- src/ch14/Room.kt
+++ src/ch14/Room.kt
@@ -1,6 +1,8 @@
 package ch14
 
-open class Room (val name: String) {
+open class Room(val name: String) {
     fun description() = "Room: $name"
     fun load() = "Nothing much to see here..."
 }
+
+class TownSquare : Room("Town Square")
