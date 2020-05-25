0b8a2bc07f20bdc4c7cf1bc2d8ddf2326967a1ee
Martin Tung <martin25tung@gmail.com>
2020-05-25 10:39:09 +0800

    初始化陷阱2

--- src/ch13/Play3.kt
+++ src/ch13/Play3.kt
@@ -1,10 +1,14 @@
 package ch13
 
 class Play3() {
+    val health = 100
+    val name: String
+    private fun firstLatter() = name[0]
     init {
         val healthBonus = health.times(3)
+        println(firstLatter())
+        name = "Madrigal"
     }
-    val health = 100
 }
 
 fun main(args: Array<String>) {
