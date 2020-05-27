185958e59d6ff66364486d50c1a318b9a22f4feb
Martin Tung <martin25tung@gmail.com>
2020-05-27 09:54:58 +0800

    對象-枚舉-在枚舉類裡定義函數

--- src/ch15/Navigation.kt
+++ src/ch15/Navigation.kt
@@ -4,7 +4,14 @@ enum class Direction(private val coordinate: Coordinate) {
     NORTH(Coordinate(0, -1)),
     EAST(Coordinate(1, 0)),
     SOUTH(Coordinate(0, 1)),
-    WEST(Coordinate(-1, 0))
+    WEST(Coordinate(-1, 0));
+
+    fun updateCoordinate(playerCoordinate: Coordinate) {
+        Coordinate(playerCoordinate.x + coordinate.x,
+            playerCoordinate.y + coordinate.y)
+    }
+
+    // Direction.EAST.updateCoordinate(Coordinate(1, 0))
 }
 
 data class Coordinate(val x: Int, val y: Int) {
