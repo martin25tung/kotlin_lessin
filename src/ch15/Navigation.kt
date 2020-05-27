26b1f42650f4563e08cb3a2310b4a55082d8947f
Martin Tung <martin25tung@gmail.com>
2020-05-27 09:52:00 +0800

    對象-枚舉-定義枚舉類構造函數

--- src/ch15/Navigation.kt
+++ src/ch15/Navigation.kt
@@ -1,7 +1,10 @@
 package ch15
 
-enum class Direction {
-    NORTH, EAST, SOUTH, WEST
+enum class Direction(private val coordinate: Coordinate) {
+    NORTH(Coordinate(0, -1)),
+    EAST(Coordinate(1, 0)),
+    SOUTH(Coordinate(0, 1)),
+    WEST(Coordinate(-1, 0))
 }
 
 data class Coordinate(val x: Int, val y: Int) {
