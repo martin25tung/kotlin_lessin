e01126c8492dcb12cd4ce2bdfae175d5aaffcfbf
Martin Tung <martin25tung@gmail.com>
2020-06-06 16:32:55 +0800

    介面與抽象類別-實現介面-聲明實現一個介面

---
+++ src/ch16/Player.kt
@@ -0,0 +1,54 @@
+package ch16
+
+import java.io.File
+
+class Player(
+    _name: String,override var healthPoints: Int = 100, var isBlessed: Boolean,
+    private val isImmortal: Boolean
+): Fightable {
+    var name = _name
+        get() = "${field.capitalize()} of $hometown"
+        private set(value) {
+            field = value.trim()
+        }
+    val hometown by lazy{ selectHometown() }
+    var currentPosition = Coordinate(0, 0)
+
+    init {
+        require(healthPoints > 0, { "healthPoints must be greater than zero." })
+        require(name.isNotBlank(), { "Player must have a name." })
+    }
+
+    constructor(name: String) : this(name, isBlessed = true, isImmortal = false) {
+        if (name.toLowerCase() == "kar") healthPoints = 40
+    }
+
+    fun castFireball(numFireball: Int = 2) =
+        println("A glass of Fireball springs into existence.(x$numFireball)")
+
+    fun auraColor() =
+        if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
+
+    fun formatHealthStatus() = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+
+    private fun selectHometown() = File("data/towns.txt").readText().split("\n").shuffled().first()
+
+    override val diceCount: Int
+        get() = TODO("Not yet implemented")
+    override val diceSides: Int
+        get() = TODO("Not yet implemented")
+
+    override fun attack(opponent: Fightable): Int {
+        TODO("Not yet implemented")
+    }
+}
