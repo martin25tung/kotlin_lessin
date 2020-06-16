03940ab952e13714bff26bf6c033c9798c10c0f9
Martin Tung <martin25tung@gmail.com>
2020-06-16 16:39:08 +0800

    介面與抽象類別-抽象類-定義一個抽象類

--- src/ch16/Creature.kt
+++ src/ch16/Creature.kt
@@ -7,9 +7,21 @@ interface Fightable {
     val diceCount: Int
     val diceSides: Int
     val damageRoll: Int
-        get() = (0 until diceCount).map{
+        get() = (0 until diceCount).map {
             Random().nextInt(diceSides) + 1
         }.sum()
 
     fun attack(opponent: Fightable): Int
+}
+
+abstract class Monster(
+    val name: String,
+    val description: String,
+    override var healthPoints: Int
+) : Fightable {
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = damageRoll
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }
 }
