bd57a1ff99d8815d6d02f2afea7e0e1a9319fdc5
Martin Tung <martin25tung@gmail.com>
2020-06-18 17:43:43 +0800

    泛型-定義泛型類-創建兩個抽獎箱

--- src/ch17/Generics.kt
+++ src/ch17/Generics.kt
@@ -3,3 +3,12 @@ package ch17
 class LootBox<T>(item: T){
     private var loot: T = item
 }
+
+class Fedora(val name: String, val value: Int)
+
+class Coin(val value: Int)
+
+fun main(args: Array<String>) {
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a greneric-looking fedora", 15))
+    val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+}
