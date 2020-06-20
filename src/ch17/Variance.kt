6127b7258fbb13c8761cffeae01669015ff60d73
Martin Tung <martin25tung@gmail.com>
2020-06-20 10:47:11 +0800

    泛型-in與out-在main函數中使用Barrel泛型類

--- src/ch17/Variance.kt
+++ src/ch17/Variance.kt
@@ -2,3 +2,7 @@ package ch17
 
 class Barrel<T>(var item: T)
 
+fun main(args: Array<String>) {
+    var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+}
