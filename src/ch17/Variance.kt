7765cf3aa07f88f26275861a2c19786cbb4522af
Martin Tung <martin25tung@gmail.com>
2020-06-20 11:31:18 +0800

    泛型-深入學習-reified關鍵字

--- src/ch17/Variance.kt
+++ src/ch17/Variance.kt
@@ -7,4 +7,16 @@ fun main(args: Array<String>) {
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
 
     fedoraBarrel = lootBarrel
+
+    randomOrBackupLoot { Fedora("a backup fedora", 15) }.run { println(name) }
+}
+
+inline fun <reified T> randomOrBackupLoot(backupLoot: () -> T) : T {
+    val items = listOf(Coin(14), Fedora("a fedora of the ages", 150))
+    val randomLoot: Loot = items.shuffled().first()
+    return if (randomLoot is T) {
+        randomLoot
+    } else {
+        backupLoot()
+    }
 }
