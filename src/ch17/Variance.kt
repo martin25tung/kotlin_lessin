b950e583922ad1b928074b1a3b69e7d07e514358
Martin Tung <martin25tung@gmail.com>
2020-06-20 10:52:24 +0800

    泛型-in與out-嘗試給lootBarrel重新賦值

--- src/ch17/Variance.kt
+++ src/ch17/Variance.kt
@@ -5,4 +5,6 @@ class Barrel<T>(var item: T)
 fun main(args: Array<String>) {
     var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+
+    lootBarrel = fedoraBarrel
 }
