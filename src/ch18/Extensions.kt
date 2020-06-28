a35759f53f995ad98e845e06d175bf18d4a1cfe5
Martin Tung <martin25tung@gmail.com>
2020-06-28 15:26:00 +0800

    擴展-定義擴展函數-擴展Any類

--- src/ch18/Extensions.kt
+++ src/ch18/Extensions.kt
@@ -2,6 +2,8 @@ package ch18
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
+fun Any.easyPrint() = println(this)
+
 fun main(args: Array<String>) {
-    println("Madrigal has left the building".addEnthusiasm())
+    "Madrigal has left the building".addEnthusiasm().easyPrint()
 }
