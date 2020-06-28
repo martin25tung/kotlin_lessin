c66c4a9e485fe793ad6c32fd82f81f4235a8ec0f
Martin Tung <martin25tung@gmail.com>
2020-06-28 15:41:10 +0800

    擴展-擴展屬性-添加擴展屬性

--- src/ch18/Extensions.kt
+++ src/ch18/Extensions.kt
@@ -2,6 +2,9 @@ package ch18
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
+val String.numVowels
+    get() = count { "aeiouy".contains(it) }
+
 fun <T>T.easyPrint() : T{
     println(this)
     return this
