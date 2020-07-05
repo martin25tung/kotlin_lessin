fda93f08403d43058bbb77074a69c9acfb74f103
Martin Tung <martin25tung@gmail.com>
2020-07-05 14:29:00 +0800

    擴展-用擴展封裝代碼-添加一個私有random擴展

---
+++ src/ch18/Tavern.kt
@@ -0,0 +1,101 @@
+package ch18
+
+import java.io.File
+import kotlin.math.roundToInt
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+
+val planets = setOf("Merrcury", "Venus", "Earth", "Earth")
+
+val lastName = listOf ("Ironfoot", "Fernsworth", "Baggins")
+
+val menuList = File("data/tavern-menu-items.txt")
+    .readText()
+    .split("\n")
+
+val uniquePatrons = mutableSetOf<String>()
+
+val patronGold = mutableMapOf<String, Double>()
+
+private fun <T> Iterable<T>.random(): T = this.shuffled().first()
+
+fun main(args: Array<String>) {
+    if (patronList.contains("Eli")) {
+        println("The tavern master says: Eli's in the back playing cards.")
+    } else {
+        println("The tavern master says: Eli isn't here.")
+    }
+
+    if (patronList.containsAll(listOf("Sophie", "Mordoc"))){
+        println("The tavern master says: Yea, they're seated by the stew kettle.")
+    } else {
+        println("The tavern master says: Nay, they departed hours ago.")
+    }
+
+//    placeOrder("shandy,Dragon's Breath, 5.91")
+
+    (0..9).forEach{
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first $last"
+        uniquePatrons += name
+    }
+
+    uniquePatrons.forEach{
+        patronGold[it] = 6.0
+    }
+
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
+        orderCount++
+    }
+
+    displayPatronBalances()
+
+}
+
+private fun displayPatronBalances(){
+    patronGold.forEach{ patron, balance ->
+        println("$patron, balance: ${"%.2f".format(balance)}")
+    }
+}
+
+fun performPurchase(price: Double, patronName: String) {
+    val totalPurse = patronGold.getValue(patronName)
+    patronGold[patronName] = totalPurse - price
+}
+
+private fun placeOrder(patronName:String, menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("$patronName speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "$patronName buys a $name ($type) for $price"
+    println(message)
+
+
+    performPurchase(price.toDouble(), patronName)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "$patronName exclaims ${toDragonSpeak("Ah, delicious $name")}"
+    } else {
+        "$patronName says: Thanks for the $name"
+    }
+    println(phrase)
+
+}
+
+private fun toDragonSpeak(phrase: String) = phrase.replace(Regex("[aeiouAEIOU]")) {
+    when (it.value) {
+        "a","A" -> "4"
+        "e", "E" -> "3"
+        "i", "I" -> "1"
+        "o", "O" -> "0"
+        "u", "U" -> "|_|"
+        else -> it.value
+    }
+}
