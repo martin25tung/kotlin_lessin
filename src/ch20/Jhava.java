06be816f51da2d3abab8d69854d344d2f415575c
Martin Tung <martin25tung@gmail.com>
2020-07-11 17:20:14 +0800

    Kotlin與Java互操作-互操作與可空性-使用@NotNull註解表明返回值不為空

--- src/ch20/Jhava.java
+++ src/ch20/Jhava.java
@@ -1,8 +1,10 @@
 package ch20;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Jhava {
+    @NotNull
     public String utterGreeting(){
         return "BLARGH";
     }
