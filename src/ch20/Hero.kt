988cf5e0fe1556a72f092e60904f1c0b6b756513
Martin Tung <martin25tung@gmail.com>
2020-07-11 17:06:42 +0800

    Kotlin與Java互操作-互操作與可空性-打印friendshipLevel變量值

--- src/ch20/Hero.kt
+++ src/ch20/Hero.kt
@@ -3,4 +3,7 @@ package ch20
 fun main(args: Array<String>) {
     val adversary = Jhava()
     println(adversary.utterGreeting())
+
+    val friendshipLevel = adversary.determineFriendshipLevel()
+    println(friendshipLevel?.toLowerCase())
 }
