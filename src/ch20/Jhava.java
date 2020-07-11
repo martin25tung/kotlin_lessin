015c7f934a6602fd97781aca8946950c8db1acf0
Martin Tung <martin25tung@gmail.com>
2020-07-11 17:08:03 +0800

    Kotlin與Java互操作-互操作與可空性-標明返回值可能為null

--- src/ch20/Jhava.java
+++ src/ch20/Jhava.java
@@ -1,10 +1,13 @@
 package ch20;
 
+import org.jetbrains.annotations.Nullable;
+
 public class Jhava {
     public String utterGreeting(){
         return "BLARGH";
     }
 
+    @Nullable
     public String determineFriendshipLevel() {
         return null;
     }
